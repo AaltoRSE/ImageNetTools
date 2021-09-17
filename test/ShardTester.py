835442bf18c1a1918af26c835e998bdd757afe06
Thomas Pfau <github@thomaspfau.de>
2021-09-17 13:42:14 +0300

    Adding test for Shard reading

--- test/ShardTester.py
+++ test/ShardTester.py
@@ -14,11 +14,16 @@ import os
 import numpy.random as random
 import tempfile
 import imageNetProvider 
+import time
+import importlib
 
 tempFolder = tempfile.TemporaryDirectory()
  
 class ShardTester(unittest.TestCase):
 
+    def setUp(self):
+        importlib.reload(imageNetProvider)
+        importlib.reload(imageNetMapper)
 
     def test_ShardProcessing(self):
         imageFolder = os.path.join('Data','Images');
@@ -33,11 +38,30 @@ class ShardTester(unittest.TestCase):
         for file in filesInTempFolder:
             assert file.startswith('Shards')            
         # Thats writing the shards done. Now read them
-        fileBases = [os.path.splitext(file)[1] for file in files];
+        fileBases = {os.path.join(imageFolder,os.path.splitext(file)[0]) for file in files}
         shardNames = os.path.join(tempFolder.name,"Shards{0..3}.tar")
         prov = imageNetProvider.imageNetProvider(shardNames, 2);
-        prov.getBatch()           
+        batch = prov.getBatch();
+        #The first batch should be of size 2. since we have multiple workers,
+        # on the data input the batch size is at most
+        assert len(batch[0])== 2
+        #lets see if all keys have been loaded
+         
+        while not batch == None:
+            assert len(batch[0])>= 1 # we can't make a stronger assertion here.
+            for key in batch[0]:
+                assert key in fileBases
+                fileBases.remove(key)            
+            print(batch[2])
+            print(batch[0])
+            batch = prov.getBatch()
+        
+        assert len(fileBases) == 0
+            
+        
+        
 
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testShardProcessing']
     unittest.main()
