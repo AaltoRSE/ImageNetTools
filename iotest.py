1664031ddd315344ecabc6ebf7f3b24c320e6c31
Thomas Pfau <github@thomaspfau.de>
2021-09-29 15:07:34 +0300

    adding entry point for IO benchmark

---
+++ iotest.py
@@ -0,0 +1,26 @@
+'''
+Created on Sep 29, 2021
+
+@author: thomas
+'''
+
+import ImageNetTools
+import sys
+import getopt
+                        
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv,"hd:",["dataset="])
+    except getopt.GetoptError:
+        printHelp()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-d", "--dataset"):            
+            ImageNetTools.benchmarkIOSpeeds(arg)     
+    sys.exit()
+   
+def printHelp():
+    print('Run IO Speed testing with a given Dataset')
+    print('python iotest.py -d /path/to/dataset' )        
+    
+main(sys.argv[1:])
