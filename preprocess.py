054f5836cc4147cd4fcccdc68fc36e580045e929
Thomas Pfau <github@thomaspfau.de>
2022-04-04 08:03:32 +0300

    Pre-process default

--- preprocess.py
+++ preprocess.py
@@ -14,9 +14,8 @@ normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
 
 image_transformations = transforms.Compose([
                                  transforms.Resize(256),
-                                 transforms.CenterCrop(256),
-                                 transforms.ToTensor(),
-                                 normalize])   
+                                 transforms.CenterCrop(256)
+                                 ])   
 
 
 def preprocess(binary_data):
@@ -24,12 +23,15 @@ def preprocess(binary_data):
     f = BytesIO(binary_data)
     # Convert it into an image, so that we can apply the transformations
     image = Image.open(f)
+    #Convert to RGB
+    image = image.convert('RGB')
     # Apply the transformations
     transformed = image_transformations(image);
     # create a BytesIO Buffer to write the tensor to.
     data = BytesIO()
-    # save into the buffer
-    torch.save(transformed,data)
+    # save compressed image into buffer. This is the best we get with a lossless compression.
+    transformed.save(data,'PNG')
+    
     # return the contents of the buffer, which is the binary object.
     # when now using the data, you will need to interpret it as a tensorflow
     # data = BytesIO(element) 
